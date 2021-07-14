328696146d0aadb1f0463e5cb673ce6b86d814ce
arkarn <arkarn2020@gmail.com>
2021-07-15 02:52:47 +0530

    init

--- src/App.js
+++ src/App.js
@@ -1,25 +1,91 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import { v4 as uuidv4 } from "uuid";
+import "./styles.css";
 
-function App() {
+export default function App() {
+  const [item, setItem] = useState("");
+  const [list, setList] = useState([]);
+
+  const addItem = (e) => {
+    e.preventDefault();
+    const newItem = { id: uuidv4(), text: item, complete: false };
+    setList([...list, newItem]);
+    setItem("");
+  };
+
+  const deleteItem = (id) => {
+    setList(list.filter((listItem) => listItem.id !== id));
+  };
+
+  const toggleItem = (id) => {
+    let toggled = list.map((listItem) =>
+      listItem.id === id
+        ? { ...listItem, complete: !listItem.complete }
+        : { ...listItem }
+    );
+    setList(toggled);
+  };
+
+  useEffect(() => {
+    console.log(list);
+  }, [list]);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h3>todo-app</h3>
+      <form onSubmit={addItem}>
+        <input
+          autoFocus
+          type="text"
+          placeholder="add an item"
+          value={item}
+          onChange={(e) => setItem(e.target.value)}
+        />
+        &nbsp;
+        <button type="submit">Add</button>
+      </form>
+      <hr />
+      <ul>
+        {list.map((listitem) => {
+          return (
+            <li
+              key={listitem.id}
+              style={{
+                padding: "5px",
+                margin: "5px",
+                width: "250px",
+                display: "flex",
+                justifyContent: "flex-start",
+                alignItems: "center",
+                border: "1px solid black"
+              }}
+            >
+              <span
+                onClick={() => toggleItem(listitem.id)}
+                style={{
+                  cursor: "pointer",
+                  color: listitem.complete ? "palevioletred" : "green"
+                }}
+              >
+                {" "}
+                {listitem.text}{" "}
+              </span>
+              <span
+                onClick={() => deleteItem(listitem.id)}
+                style={{
+                  marginLeft: "auto",
+                  cursor: "pointer",
+                  fontSize: "12px"
+                }}
+              >
+                {" "}
+                remove{" "}
+              </span>
+            </li>
+          );
+        })}
+      </ul>
     </div>
   );
 }
 
-export default App;
